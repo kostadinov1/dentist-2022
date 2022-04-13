0fa024af90baf94f484d3d5009e79b79b4a751bf
kostadinov1 <evgenikostadinov1987@gmail.com>
2022-04-13 20:49:52 +0300

    modified services template styling

--- dentist_3_project/core/views/generic.py
+++ dentist_3_project/core/views/generic.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'core/index.html'
 
+    # tried to cache the page with no success. --- HAVE A LOOK AGAIN ---
+    # @method_decorator(cache_page(60 * 5), name='dispatch')
+    # def dispatch(self, request, *args, **kwargs):
+    #     return super().dispatch(*args, **kwargs)
 
 
 class ContactsView(TemplateView):
